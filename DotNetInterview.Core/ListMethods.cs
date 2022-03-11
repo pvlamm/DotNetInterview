7781d33ec167cb3c820cbba60a8579d77d148404
Patrick Lamm <lammster@gmail.com>
2022-03-11 08:56:57 -0500

    check point

---
+++ DotNetInterview.Core/ListMethods.cs
@@ -0,0 +1,40 @@
+﻿namespace DotNetInterview.Core
+{
+    public static class ListMethods
+    {
+        /// <summary>
+        /// Given a list of numbers, return the List, sorted
+        /// </summary>
+        /// <param name="unsortedNumbers"></param>
+        /// <returns></returns>
+        public static IEnumerable<int> SortNumbers(IEnumerable<int> unsortedNumbers)
+        {
+            return unsortedNumbers;
+        }
+
+        /// <summary>
+        /// Given a List of Numbers, get the nthLargest number
+        /// Example: Given Array { 1, 3, 8, 10, 15 }, return the 2nd Largest Number: 10
+        /// Example 2: Given Array { 1, 3, 8, 10, 15 }, return the 6th Largest Number breaks, return -1
+        /// </summary>
+        /// <param name="numbers"></param>
+        /// <param name="nthLargest"></param>
+        /// <returns></returns>
+        public static int GetNthLargestElement(IEnumerable<int> numbers, int nthLargest)
+        {
+            return numbers.First();
+        }
+
+        /// <summary>
+        /// Given two Lists of type T, merge the contents into a single list
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="ListA"></param>
+        /// <param name="ListB"></param>
+        /// <returns></returns>
+        public static IEnumerable<T> MergeLists<T>(IEnumerable<T> ListA, IEnumerable<T> ListB)
+        {
+            return null;
+        }
+    }
+}
---
+++ DotNetInterview.Tests/ListMethodsTests.cs
@@ -0,0 +1,39 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace DotNetInterview.Tests
+{
+    [TestClass]
+    public class ListMethodsTests
+    {
+        private readonly int[] _unsortedNumbers = new int[15] { 510, 320, 240, 12, 7, 300, 241, 420, 8042, 1104, 294, 2, 0, 7, 99 };
+        private readonly int[] _sortedNumbers = new int[15] { 0, 2, 7, 7, 12, 99, 240, 241, 294, 300, 320, 420, 510, 1104, 8042 };
+
+        [TestMethod]
+        public void DotNetInterview_Core_SortNumbers_SortsCorrectly()
+        {
+            var result = DotNetInterview.Core.ListMethods.SortNumbers(_unsortedNumbers).ToArray();
+
+            CollectionAssert.AreEqual(_sortedNumbers, result);
+        }
+
+        [TestMethod]
+        public void DotNetInterview_Core_GetNthLargestElement_Gets2ndLargestCorectly()
+        {
+            var answer = 1104;
+
+            var result = Core.ListMethods.GetNthLargestElement(_unsortedNumbers, 2);
+
+            Assert.AreEqual(answer, result);
+        }
+
+        [TestMethod]
+        public void DotNetInterview_Core_GetNthLargestElement_GetsNthBeyondRange()
+        {
+            var answer = -1;
+            var result = Core.ListMethods.GetNthLargestElement(_unsortedNumbers, _unsortedNumbers.Count());
+
+            Assert.AreEqual(answer, result);
+        }
+    }
+}
